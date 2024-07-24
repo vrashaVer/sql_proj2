ad03d499466dfcf8939edca70039f5d4f025e381
vrashaVer <da.programs.vr@gmail.com>
2024-07-24 10:18:08 +0300

    create sql table
    create file where sql table

---
+++ create_db.sql
@@ -0,0 +1,23 @@
+CREATE TABLE products (
+    product_id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    category TEXT NOT NULL,
+    price REAL NOT NULL
+);
+
+
+CREATE TABLE customers ( customer_id INTEGER PRIMARY KEY, 
+    first_name TEXT NOT NULL, 
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE 
+);
+
+
+CREATE TABLE orders ( order_id INTEGER PRIMARY KEY, 
+    customer_id INTEGER NOT NULL, 
+    product_id INTEGER NOT NULL, 
+    quantity INTEGER NOT NULL, 
+    order_date DATE NOT NULL, 
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id), 
+    FOREIGN KEY (product_id) REFERENCES products(product_id) 
+);
